4a51ef9af3207fbd0426e8faaf95e484425a008c
kugaitomomi <tottomo7@gmail.com>
2022-08-23 21:46:35 +0900

    chore: prepare documantion

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,22 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Register from "./components/Register";
+// import Login from "./components/Login";
+import Upload from "./components/Upload";
 import './App.css';
 
-function App() {
-  const [pic, setPic] = useState();
-  const [allPics, setAllPics] = useState([]);
-  useEffect(() => {
-    getAllPics();
-  }, [allPics]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("pic", pic);
-    await axios
-      .post("http://localhost:5000/addPicture", formData)
-      .then(getAllPics())
-      .catch((error) => console.log(error.message));
-  };
-  const handleChange = (e) => {
-    setPic(e.target.files[0]);
-  };
-
-  const getAllPics = async () => {
-    await axios
-      .get("http://localhost:5000/pictures")
-      .then((res) => setAllPics(res.data))
-      .catch((error) => console.log(error.message));
-  };
-
-  const handleDelete = async (name) => {
-    await axios
-      .delete("http://localhost:5000/delete", {
-        data: { name: name },
-      })
-      .then(getAllPics())
-      .catch((error) => console.log(error.message));
-  };
-
+const App = () => {
   return (
-    <div className="app">
-      <form className="form" onSubmit={handleSubmit}>
-        <input type="file" onChange={handleChange} />
-        <button>upload</button>
-      </form>
-      <div className="imgsContainer">
-        {allPics &&
-          allPics.map((p) => (
-            <div className="imgItem" key={p.name}>
-              <img className="img" src={p.url} alt="" />
-              <button
-                className="imgButton"
-                onClick={() => handleDelete(p.name)}
-              >
-                Delete
-              </button>
-            </div>
-          ))}
-      </div>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path={`/register/`} element={<Register />} />
+          <Route path={`/login/`} element={<Login />} /> */}
+          <Route path={`/`} element={<Upload />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
