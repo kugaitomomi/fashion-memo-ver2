bdf88370076511c8a76557d964c71a0c1c703c91
kugaitomomi <tottomo7@gmail.com>
2022-08-23 19:18:25 +0900

    feat: make mainpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [pic, setPic] = useState();
+  const [allPics, setAllPics] = useState([]);
+  useEffect(() => {
+    getAllPics();
+  }, [allPics]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("pic", pic);
+    await axios
+      .post("http://localhost:5000/addPicture", formData)
+      .then(getAllPics())
+      .catch((error) => console.log(error.message));
+  };
+  const handleChange = (e) => {
+    setPic(e.target.files[0]);
+  };
+
+  const getAllPics = async () => {
+    await axios
+      .get("http://localhost:5000/pictures")
+      .then((res) => setAllPics(res.data))
+      .catch((error) => console.log(error.message));
+  };
+
+  const handleDelete = async (name) => {
+    await axios
+      .delete("http://localhost:5000/delete", {
+        data: { name: name },
+      })
+      .then(getAllPics())
+      .catch((error) => console.log(error.message));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="file" onChange={handleChange} />
+        <button>upload</button>
+      </form>
+      <div className="imgsContainer">
+        {allPics &&
+          allPics.map((p) => (
+            <div className="imgItem" key={p.name}>
+              <img className="img" src={p.url} alt="" />
+              <button
+                className="imgButton"
+                onClick={() => handleDelete(p.name)}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
